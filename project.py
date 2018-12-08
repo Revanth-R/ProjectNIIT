7f6cb100f2931e7a9c83fc75e01af4512e61b056
Revanth-R <revanth1409@gmail.com>
2018-12-08 11:46:03 +0530

    second commit

--- project.py
+++ project.py
@@ -1,1 +1,20 @@
-import
+#level_2
+x=input("Enter the url:")
+word=str(input("Enter the word:"))
+num=0
+import requests
+from bs4 import BeautifulSoup
+r=requests.get(x)
+soup = BeautifulSoup(str(r.text),'html.parser')
+html_doc=str(r.text)
+with open('count.txt','w') as file:
+    file.write(html_doc)
+with open('count.txt','r') as f:
+    for line in f:
+        words=line.split()
+        for i in words:
+            if(i==word):
+                num=num+1
+print("The Count of word is:",num)            
+        
+        
